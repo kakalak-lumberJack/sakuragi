85e9878ccab32b7b7a3465eaf16608a0a293c7e6
Jack Manning <jwmannin@gmail.com>
2021-02-03 04:06:35 -0500

    Fix trees being placed 2 nodes deep on mapgen

--- mapgen.lua
+++ mapgen.lua
@@ -1,32 +1,8 @@
---[[minetest.register_on_generated( function(minp, maxp)
-	--local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
-	--local area = VoxelArea:new({MinEdge = emin, MaxEdge = emax})
-	--local data = vm:get_data()
-	if maxp.y > 0 then
-		local under_air = minetest.find_nodes_in_area_under_air(minp, maxp, {"bamboo_forest:dirt_with_bamboo_leaf_litter"})
-		for _, pos in ipairs(under_air) do 
-			if math.random() < 0.0004 then
-				minetest.remove_node(pos)
-				minetest.place_schematic(pos, sakuragi.cherry_tree_schematic, "random", nil, true, {place_center_x = true, place_center_z = true})
-			end
-		end
-	
-	
-		--if data[index] == minetest.get_content_id("bamboo_forest:dirt_with_bamboo_leaf_litter") then
-		
-		--if math.random() < 0.0004 then 
-			--minetest.log(minetest.get_biome_data(pos).biome)
-			--minetest.place_schematic_on_vmanip(vm, pos, sakuragi.cherry_tree_schematic, "random", nil, false)
-		--end
-	end
-end)]]--
-
 
 minetest.register_decoration({
     name = "sakuragi:cherry_tree",
     deco_type = "schematic",
     place_on = {"bamboo_forest:dirt_with_bamboo_leaf_litter", "default:dirt_with_grass"},
-    place_offset_y = -1,
     sidelen = 16,
     --fill_ratio = 0.0004,
     noise_params = {
